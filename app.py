21185bb24e2039fd6d579f357d40757bfb077fec
RomuloVit <romulovitoria@gmail.com>
2025-01-06 10:30:25 -0300

    correção aspas

--- app.py
+++ app.py
@@ -668,8 +668,8 @@ app.layout = dbc.Container(
                         html.B("Titulo - 123456789 123456789 123456789 123456789 123456789", style={"color": "#666", "height": "30px"}),
                         html.P("Subtitulo - 123456789 123456789 123456789 123456789 123456789", style={"color": "#666", "gap": "10px","height": "20px"}),
                         dcc.Graph(figure=fig_bairro, id="graph", responsive=True, style={"height": "50vh", "marginBottom": "5px"},config={"displayModeBar": False}),
-                        style={"backgroundColor": "white","marginBottom": "20px", "padding": "10px", "borderRadius": "5px"}
-                    ),
+                        ),
+                style={"backgroundColor": "white","marginBottom": "20px", "padding": "10px", "borderRadius": "5px"},
                 sm=11,md=5),
                 dbc.Col(
                     dbc.Card(
